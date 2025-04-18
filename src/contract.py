fae961d58806304426885c6ed1fb88ee0fc1561a
Victor Phan <vanphandinh@outlook.com>
2025-04-19 00:52:19 +0700

    fix rpc

--- src/contract.py
+++ src/contract.py
@@ -6,9 +6,10 @@ from itertools import cycle
 
 load_dotenv()
 
-wss_rpc_endpoints = [
-    "wss://base-rpc.publicnode.com",
-    "wss://0xrpc.io/base"
+rpc_endpoints = [
+    "https://lb.drpc.org/ogrpc?network=base&dkey=Avibgvi26EjPsw76UtdwmsS6VEL-8F4R75KJIhIl_7lF",
+    "https://base-rpc.publicnode.com",
+    "https://base.rpc.subquery.network/public"
 ]
 sugar_lp_address = Web3.to_checksum_address(os.getenv("SUGAR_LP_ADDRESS"))
 price_oracle_address = Web3.to_checksum_address(os.getenv("PRICE_ORACLE_ADDRESS"))
@@ -23,11 +24,11 @@ with open('./abi/OffchainOracle.json', 'r') as abi_file:
     price_oracle_abi = json.load(abi_file)    
 
 
-rpc_cycle = cycle(wss_rpc_endpoints)
+rpc_cycle = cycle(rpc_endpoints)
 
 def get_web3():
     url = next(rpc_cycle)
-    return Web3(Web3.WebsocketProvider(url))
+    return Web3(Web3.HTTPProvider(url))
 
 
 web3 = get_web3()
